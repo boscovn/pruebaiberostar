0cff81f8ab32f8b288d6e8cf2bfd6ad1bfed2ad1
bosco <bosco@vallejonagera.xyz>
2023-04-21 20:57:41 +0200

    Centralised exceptions

---
+++ src/main/java/com/example/demo/handler/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.example.demo.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        String message = "There was an error in the server";
+        return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
