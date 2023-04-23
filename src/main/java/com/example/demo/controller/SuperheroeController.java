8b10c6a270f78cc4263d70c689dc08d73195fca8
bosco <bosco@vallejonagera.xyz>
2023-04-23 16:13:16 +0200

    Documented the application

--- src/main/java/com/example/demo/controller/SuperheroeController.java
+++ src/main/java/com/example/demo/controller/SuperheroeController.java
@@ -3,6 +3,12 @@ package com.example.demo.controller;
 import com.example.demo.annotations.LogExecutionTime;
 import com.example.demo.model.Superheroe;
 import com.example.demo.repository.SuperheroeRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
@@ -19,6 +25,13 @@ public class SuperheroeController {
     @Autowired
     SuperheroeRepository superheroeRepository;
 
+    @Operation(summary = "Get a list of all superheroes or some superheroes that contain a substring in its name")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "List of superheroes",
+                    content = {@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = Superheroe.class)))}),
+            @ApiResponse(responseCode = "204", description = "No superheroes match criteria",
+                    content = @Content)
+    })
     @GetMapping("/superheroes")
     @Cacheable("myCache")
     @LogExecutionTime
@@ -30,6 +43,13 @@ public class SuperheroeController {
         else return new ResponseEntity<>(superheroes, HttpStatus.OK);
     }
 
+    @Operation(summary = "Get a superhero given a specified id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Superhero",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Superheroe.class))}),
+            @ApiResponse(responseCode = "404", description = "No superhero with that ID",
+                    content = @Content)
+    })
     @GetMapping("/superheroes/{id}")
     @Cacheable("myCache")
     @LogExecutionTime
@@ -39,11 +59,21 @@ public class SuperheroeController {
     }
 
     @PostMapping("/superheroes")
+    @ApiResponse(responseCode = "200", description = "Superhero we just created",
+            content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Superheroe.class))})
+    @Operation(summary = "Create a superheroe")
     @LogExecutionTime
-    public ResponseEntity<Superheroe> createTutorial(@RequestBody Superheroe superheroe) {
+    public ResponseEntity<Superheroe> createSuperheroe(@RequestBody Superheroe superheroe) {
         return new ResponseEntity<>(superheroeRepository.save(new Superheroe(superheroe.getName())), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Update a superhero given a specified id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Superhero",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Superheroe.class))}),
+            @ApiResponse(responseCode = "404", description = "No superhero with that ID",
+                    content = @Content)
+    })
     @PutMapping("/superheroes/{id}")
     @LogExecutionTime
     public ResponseEntity<Superheroe> updateSuperheroe(@PathVariable("id") long id, @RequestBody Superheroe superheroe) {
@@ -58,6 +88,9 @@ public class SuperheroeController {
         }
     }
 
+    @Operation(summary = "Delete a superhero given a specified id")
+    @ApiResponse(responseCode = "204", description = "Either it was deleted or it didn't exist to begin with",
+            content = @Content)
     @DeleteMapping("/superheroes/{id}")
     @LogExecutionTime
     public ResponseEntity<HttpStatus> deleteSuperheroe(@PathVariable("id") long id) {
